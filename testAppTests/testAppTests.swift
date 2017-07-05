f4e08a6d44f6532714c8fdde2b9f63ddea974a8b
Chaitanya <chaitanyapandit@me.com>
2017-07-05 13:00:22 +0530

    Update test

--- testAppTests/testAppTests.swift
+++ testAppTests/testAppTests.swift
@@ -13,6 +13,9 @@ class testAppTests: XCTestCase {
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        let expectation1 = expectation(description: "Creating User")
+        expectation1.fulfill()
+
     }
     
     override func tearDown() {
