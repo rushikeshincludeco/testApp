d8aed9b19004a03401b4ced1b7d047c65c6cf278
Chaitanya <chaitanyapandit@me.com>
2017-07-05 15:18:42 +0530

    Update add key

--- scripts/add-key.sh
+++ scripts/add-key.sh
@@ -3,9 +3,7 @@
 KEYCHAIN_PASSWORD=circleci
 
 security create-keychain -p $KEYCHAIN_PASSWORD ios-build.keychain
-security import ./scripts/apple.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
-security import ./scripts/dist.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
-security import ./scripts/dist.p12 -k ~/Library/Keychains/ios-build.keychain -P $KEY_PASSWORD -T /usr/bin/codesign
+security import ./scripts/1.p12 -k ~/Library/Keychains/ios-build.keychain -P $KEY_PASSWORD -T /usr/bin/codesign
 security list-keychain -s ~/Library/Keychains/ios-build.keychain
 security unlock-keychain -p $KEYCHAIN_PASSWORD ~/Library/Keychains/ios-build.keychain
 
