a5999ce56b27d6db2d5b6825fe4028db1668c963
Chaitanya <chaitanyapandit@me.com>
2017-07-07 00:24:05 +0530

    2

--- scripts/deploy-hockeyapp.sh
+++ scripts/deploy-hockeyapp.sh
@@ -9,9 +9,9 @@ echo "Deployment Started!"
 # First paramter expected as APK path 
 #$CIRCLE_ARTIFACTS/app/outputs/apk/app-debug.apk
 
-APK_PATH=$1
+IPA_PATH=$1
 
-echo $APK_PATH
+echo $IPA_PATH
 
 # Second parameter expected as access token
 ACCESS_TOKEN=$2
@@ -24,14 +24,12 @@ APP_ID=$3
 echo $APP_ID
 
 curl \
- -F "status=2" \
- -F "notify=1" \
- -F "notes=Some new features and fixed bugs." \
- -F "notes_type=0" \
- -F "ipa=@hockeyapp.ipa" \
- -F "dsym=@hockeyapp.dSYM.zip" \
- -H "X-HockeyAppToken: $HOCKEYAPP_TOKEN" \
- https://rink.hockeyapp.net/api/2/apps/upload
+-F "status=2" \
+-F "notify=0" \
+-F "notes=CircleCI Build" \
+-F "ipa=@$APK_PATH" \
+-H "X-HockeyAppToken:$ACCESS_TOKEN" \
+https://rink.hockeyapp.net/api/2/apps/$APP_ID/app_versions/upload
 
 echo "Deployment finished!"
 
